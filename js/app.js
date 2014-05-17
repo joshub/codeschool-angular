c5c8b7df0254ea38a2ba12193cbfab86c97e09f9
Yuichi Hagio <yhagio87@gmail.com>
2014-05-17 11:49:34 +0900

    custom directive panaels, index html becomes clean

--- js/app.js
+++ js/app.js
@@ -47,17 +47,17 @@
   }];
 
   //  Panel Controller
-  app.controller('PanelController', function(){
-    this.tab = 1;
+  // app.controller('PanelController', function(){
+  //   this.tab = 1;
 
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
+  //   this.selectTab = function(setTab){
+  //     this.tab = setTab;
+  //   };
 
-    this.isSelected = function(checkTab){
-      return this.tab == checkTab;
-    };
-  });
+  //   this.isSelected = function(checkTab){
+  //     return this.tab == checkTab;
+  //   };
+  // });
 
 
   // Review Controller
@@ -76,6 +76,25 @@
       restrict: 'A',
       templateUrl: 'product_title.html'
     };
+  });
+
+  app.directive('productPanels', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product_panels.html',
+      controller: function(){
+        this.tab = 1;
+
+        this.selectTab = function(setTab){
+          this.tab = setTab;
+        };
+
+        this.isSelected = function(checkTab){
+          return this.tab == checkTab;
+        };
+      },
+      controllerAs: 'panel'
+    }
   });
 
 })();
